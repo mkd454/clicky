07f66b6e84696b4f68f1bda2a7caa7ed97ccc8f3
Mary Dang <dangmaryk@gmail.com>
2019-04-29 16:23:30 -0500

    Updates

---
+++ precache-manifest.4de0f8877925e57d70831740c75dc38d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae1c683507d1671d5c80090231878c3",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "00a0c53f108b37e49f23",
+    "url": "/clicky/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clicky/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "00a0c53f108b37e49f23",
+    "url": "/clicky/static/js/main.0a5e40c4.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky/static/media/logo.5d5d9eef.svg"
+  }
+]);
